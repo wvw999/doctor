9f0b7e7bd5086c59af2911ab22169002cf71b936
Student <example@example.com>
2018-08-10 11:56:44 -0700

    add cleaner output of results

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import './styles.css';
 import { Provider } from './provider.js'
 import { Ailment } from './ailment.js'
 
+function phoneFix(number) {
+  var initialInput = (""+number).replace(/\D/g, '');
+  var updatedNumber = initialInput.match(/^(\d{3})(\d{3})(\d{4})$/);
+  return (!updatedNumber) ? null : "(" + updatedNumber[1] + ") " + updatedNumber[2] + "-" + updatedNumber[3];
+}
+
 $(document).ready(function() {
 
   let doctorList = new Provider();
@@ -17,9 +23,21 @@ $(document).ready(function() {
     $("#outputRow").html("");
     promiseOne.then(function(response) {
       let body = JSON.parse(response);
-      $("#outputRow").append(`<h3>Doctor Results</h3>`);
+      $("#outputRow").append(`<h3>Doctor Search Results</h3>`);
       for(let i = 0; i < body.data.length; i++) {
-        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${body.data[i].practices[0].phones[0].number}<br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br><a href="${body.data[i].practices[0].website}">website</a></div><br>`);
+        let websiteData;
+        let phone;
+        if (body.data[i].practices[0].website == undefined) {
+          websiteData = "no website";
+        } else {
+          websiteData = `<a href="${body.data[i].practices[0].website}">website</a>`;
+        }
+        if (body.data[i].practices[0].phones[0].number ==  undefined ) {
+          phone = "no phone number listed";
+        } else {
+          phone = phoneFix(body.data[i].practices[0].phones[0].number);
+        }
+        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${phone} <br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br>${websiteData}</div><br>`);
       }
     }, (error) => {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
