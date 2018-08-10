5bdda71e52091290752c7af8ab55b1826842259d
Student <example@example.com>
2018-08-10 12:33:14 -0700

    drying up main.js

--- src/main.js
+++ src/main.js
@@ -53,7 +53,19 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       $("#outputRow").append(`<h3>Doctors who can treat your issue:</h3>`);
       for(let i = 0; i < body.data.length; i++) {
-        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${body.data[i].practices[0].phones[0].number}<br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br><a href="${body.data[i].practices[0].website}">website</a></div><br>`);
+        let websiteData;
+        let phone;
+        if (body.data[i].practices[0].website == undefined) {
+          websiteData = "no website";
+        } else {
+          websiteData = `<a href="${body.data[i].practices[0].website}">website</a>`;
+        }
+        if (body.data[i].practices[0].phones[0].number ==  undefined ) {
+          phone = "no phone number listed";
+        } else {
+          phone = phoneFix(body.data[i].practices[0].phones[0].number);
+        }
+        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${phone} <br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br>${websiteData}</div><br>`);
       }
     }, (error) => {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
