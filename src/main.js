ab9ca093a9ac90a78083f2ee58907dfb6add7856
Student <example@example.com>
2018-08-10 11:25:24 -0700

    add basic functionality complete. refactoring now

--- src/main.js
+++ src/main.js
@@ -8,11 +8,38 @@ import { Ailment } from './ailment.js'
 $(document).ready(function() {
 
   let doctorList = new Provider();
-  let nameSearch = doctorList.getProviderAll(name);
+  let ailmentList = new Ailment();
 
+  $('#doctorName').submit((event) => {
+    event.preventDefault();
+    let docName = $('#docName').val();
+    let promiseOne = doctorList.getProviderAll(docName);
+    $("#outputRow").html("");
+    promiseOne.then(function(response) {
+      let body = JSON.parse(response);
+      $("#outputRow").append(`<h3>Doctor Results</h3>`);
+      for(let i = 0; i < body.data.length; i++) {
+        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${body.data[i].practices[0].phones[0].number}<br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br><a href="${body.data[i].practices[0].website}">website</a></div><br>`);
+      }
+    }, (error) => {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 
-
-
-
+  $('#ailmentName').submit((event) => {
+    event.preventDefault();
+    let ailmentName = $('#ailmentName').val();
+    let promiseTwo = ailmentList.getAilmentAll(ailmentName);
+    $("#outputRow").html("");
+    promiseTwo.then(function(response) {
+      let body = JSON.parse(response);
+      $("#outputRow").append(`<h3>Doctors who can treat your issue:</h3>`);
+      for(let i = 0; i < body.data.length; i++) {
+        $("#outputRow").append(`<div class="container listing">${body.data[i].practices[0].name}<br>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state},  ${body.data[i].practices[0].visit_address.zip} <br> ${body.data[i].practices[0].phones[0].number}<br>accepts new patients: ${body.data[i].practices[0].accepts_new_patients}<br><a href="${body.data[i].practices[0].website}">website</a></div><br>`);
+      }
+    }, (error) => {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 
 });
